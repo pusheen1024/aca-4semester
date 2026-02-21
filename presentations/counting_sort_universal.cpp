1a8c28d78e9b14d54a57368e42c631aa026a1e51
pusheen1024 <rita26-10@mail.ru>
2026-02-21 19:47:14 +0400

    feat: complete presentation on counting sort

--- presentations/counting_sort_universal.cpp
+++ presentations/counting_sort_universal.cpp
@@ -1,19 +1,12 @@
-// сортировка подсчётом для массива, где числа лежат в диапазоне [k1, k2], 
-// где k1, k2 - любые целые числа, в т.ч. отрицательные
-
-void counting_sort_universal(vector<int> &a) { 
-	int n = a.size();
+void counting_sort_universal(vector<int> &a, int n) { 
 	int mn = *min_element(a.begin(), a.end());
 	int mx = *max_element(a.begin(), a.end());
 	vector<int> cnt(mx - mn + 1);
-	for (int i = 0; i < n; i++) {
-		cnt[a[i] - mn]++;
-	}
+	for (int i = 0; i < n; i++) cnt[a[i] - mn]++;
 	int idx = 0;
 	for (int num = 0; num <= mx - mn; num++) {
 		for (int c = 0; c < cnt[num]; c++) {
-			a[idx] = num + mn;
-			idx++;
+			a[idx++] = num + mn;
 		}
 	}
 }
